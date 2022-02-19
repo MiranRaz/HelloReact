3624785d60bfac864cbe9be5b1d6aa72da903c96
miranraznatovic <miran.raznatovic@edu.fit.ba>
2022-02-19 08:41:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Backdrop from "./components/Backdrop";
+import Modal from "./components/Modal";
+import ToDo from "./components/ToDo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My To-Dos!</h1>
+      <ToDo text="Learn React" />
+      <ToDo text="Master React" />
+      <ToDo text="Arula" />
+      <Modal/>
+      <Backdrop/>
     </div>
   );
 }
---
+++ src/components/Modal.js
@@ -0,0 +1,11 @@
+function Modal() {
+    return( 
+    <div className="modal">
+        <p>Are you sure?</p>
+        <button className="btn ">Yes</button>
+        <button className="btn btn--alt">No</button>
+    </div>
+    );
+}
+
+export default Modal;
